3569e6759407e1c9671ff34c0f76047a8e3609df
Kauacnok <kauaprofessionalcontact@gmail.com>
2022-10-07 21:31:02 -0300

    Using redis too on component Character Full (sure)

--- web/src/pages/CharacterFull.tsx
+++ web/src/pages/CharacterFull.tsx
@@ -9,26 +9,25 @@ export function CharacterFull() {
 
 	const [character, setCharacter] = useState<any[]>([]);
 
-	let character_id_get = 0
+	const [character_ID, setCharacterID] = useState(0)
 
 	const getApiData = async () => {
 	  const response = await fetch(
 	    `${import.meta.env.VITE_API_URL}/CharactersList`
 	  ).then((response) => response.json());
 
+	  response.map((character_u: any, index: number) => {
+			if (character_u.id == Number(id)) {
+				setCharacterID(Number(index))					
+			}
+		});
+
 	  return setCharacter(response);
 	}
 
 	useEffect(() => {
-		getApiData();
-		const Character_ID = character.map((character_u: any) => {
-		if (Number(character_u.id) === Number(id)) {
-			character_id_get = Number(id)
-		}
-	})
-	}, [])
-
-	console.log(character_id_get)
+		getApiData()
+	}, [character_ID, setCharacterID])
 
 	return (
 		<>
@@ -39,22 +38,22 @@ export function CharacterFull() {
 			</nav>
 			<main>
 				<article className="flex flex-col p-5 bg-gray-700 border-2 mx-2 mb-4 border-gray-500">
-					<h2 className="text-[30px]">{character[character_id_get]?.name}</h2>
-					<span className="ml-2">Vida: {character[character_id_get]?.life}</span>
-					<p className="ml-2">Profissão: {character[character_id_get]?.role}</p>
+					<h2 className="text-[30px]">{character[character_ID]?.name}</h2>
+					<span className="ml-2">Vida: {character[character_ID]?.life}</span>
+					<p className="ml-2">Profissão: {character[character_ID]?.role}</p>
 				</article>
 				<article className="flex flex-col p-5 bg-gray-700 border-2 mx-2 mb-4 border-gray-500">
 					<h2 className="text-[30px]">Atributos:</h2>
-					<p className="ml-2">Força: {character[character_id_get]?.force}</p>
-					<p className="ml-2">Inteligência: {character[character_id_get]?.inteligence}</p>
-					<p className="ml-2">Constituição: {character[character_id_get]?.constitution}</p>
-					<p className="ml-2">Agilidade: {character[character_id_get]?.agility}</p>
-					<p className="ml-2">Charme: {character[character_id_get]?.charisma}</p>
-					<p className="ml-2">Velocidade: {character[character_id_get]?.velocity}</p>
+					<p className="ml-2">Força: {character[character_ID]?.force}</p>
+					<p className="ml-2">Inteligência: {character[character_ID]?.inteligence}</p>
+					<p className="ml-2">Constituição: {character[character_ID]?.constitution}</p>
+					<p className="ml-2">Agilidade: {character[character_ID]?.agility}</p>
+					<p className="ml-2">Charme: {character[character_ID]?.charisma}</p>
+					<p className="ml-2">Velocidade: {character[character_ID]?.velocity}</p>
 				</article>
 				<article className="flex flex-col p-5 bg-gray-700 border-2 mx-2 mb-4 border-gray-500">
 					<h2 className="text-[30px]">Inventário: </h2>
-					<textarea value={character[character_id_get]?.inventory} readOnly className="bg-gray-700 w-[90%] h-[400px]"></textarea>
+					<textarea value={character[character_ID]?.inventory} readOnly className="bg-gray-700 w-[90%] h-[400px]"></textarea>
 				</article>
 			</main>
 		</>
