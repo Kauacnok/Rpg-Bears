ad69493f86cfbfad2da2bd5871bc53e311dde944
Kauacnok <kauaprofessionalcontact@gmail.com>
2022-12-07 14:42:28 -0300

    improving alert message when database is loading or offline

--- web/src/pages/CharactersList.tsx
+++ web/src/pages/CharactersList.tsx
@@ -13,20 +13,33 @@ interface CharacterProps {
 
 export function CharactersList() {
     const [characters, setCharacters] = useState<any[]>([]);
+    const [messageAlert, setMessageAlert] = useState('Carregando o banco de dados')
 
     let categoryList: string[] = []
 
 	const getApiData = async () => {
-	  const response = await fetch(
-	    `${import.meta.env.VITE_API_URL}/CharactersList`
-	  ).then((response) => response.json());
+		try {
+			const response = await fetch(
+				`${import.meta.env.VITE_API_URL}/CharactersList/`
+			).then(
+				(response) => response.json().then(data => ({statusOk: response.ok, body: data}))
+			)
 
-	  return setCharacters(response);
+			if (response.statusOk === false) {
+				setMessageAlert('O banco de dados está offline')
+			}
+			return setCharacters(response.body);
+		} catch (error) {
+			setMessageAlert('O banco de dados está offline')
+			console.log(error)
+		}
+		
 	}
 
 	useEffect(() => {
 		getApiData();
 	}, []);
+
 
 	return (
 		<>
@@ -39,7 +52,7 @@ export function CharactersList() {
 							return (
 								<CharacterSimpleCard key={character.id} id={character.id} name={character.name} createdAt={character.createdAt} />
 							)
-						}) : <span className="self-center p-10 my-5 mx-5 border-[1px] bg-gray-500 border-gray-500 rounded-tl-lg rounded-br-lg">O banco de dados está offline</span>
+						}) : <span className="self-center p-10 my-5 mx-5 border-[1px] bg-gray-500 border-gray-500 rounded-tl-lg rounded-br-lg">{messageAlert}</span>
 					}		
 				</article>
 			</main>
